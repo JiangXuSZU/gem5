0bb75ed276d4846de3eb6252e47352b3b759611a
jiangXu <jiangxu_szu@qq.com>
2022-01-18 15:48:52 +0800

    resources: add GShareBP
    Change-Id: I661cff0a4256bb01e20a665bfbed266dc10e45f5

--- src/cpu/pred/gshare.cc
+++ src/cpu/pred/gshare.cc
@@ -1,9 +1,8 @@
 #include "cpu/pred/gshare.hh"
-
+#include "base/bitfield.hh"
 #include "base/intmath.hh"
-#include "base/logging.hh"
-#include "base/trace.hh"
 #include "debug/Fetch.hh"
+#include "debug/GShare.hh"
 
 namespace gem5
 {
@@ -13,98 +12,98 @@ namespace branch_prediction
 
 GShareBP::GShareBP(const GShareBPParams &params)
     : BPredUnit(params),
-      globalHistoryReg(params.numThreads,0),
+      globalHistoryReg(params.numThreads, 0),
       globalHistoryBits(ceilLog2(params.counterSize)),
-      counter(params.counterSize,SatCounter8(params.ctrBits)),
       ctrBits(params.ctrBits),
-      counterSize(params.counterSize)
+      counterSize(params.counterSize),
+      counters(counterSize,SatCounter8(ctrBits))
 {
-    if (!isPowerOf2(counterSize)){
-        fatal("Invalid gshare pht size!\n");
-    }
+    if (!isPowerOf2(counterSize))
+        fatal("Invalid counter size.\n");
 
-    globalHistoryMask=counterSize-1;
-    indexMask = counterSize-1;
+    historyRegisterMask = mask(globalHistoryBits);
+    globalHistoryMask = counterSize - 1;
 
-    DPRINTF(Fetch,"GHR bit: %i\n",globalHistoryBits);
-    DPRINTF(Fetch,"Counter Size: %i\n",counterSize);
-    DPRINTF(Fetch,"Counter Bits: %i\n",ctrBits);
-    DPRINTF(Fetch,"index mask: %#x\n",indexMask);
-}//GShareBP::GShareBP
-
-void
-GShareBP::squashed(ThreadID tid, void *bp_history)
-{
-    BPHistory *history = static_cast<BPHistory*>(bp_history);
-    globalHistoryReg[tid] = history->globalHistoryReg;
-
-    delete history;
+    DPRINTF(GShare,"globalHistoryBits:%i\n",globalHistoryBits);
+    DPRINTF(GShare,"ctrBits:%i\n",ctrBits);
+    DPRINTF(GShare,"counterSize:%i\n",counterSize);
+    DPRINTF(GShare,"historyRegisterMask:%#x\n",historyRegisterMask);
 }
 
 void
-GShareBP::btbUpdate(ThreadID tid, Addr branch_addr, void * &bp_history)
-{
-    globalHistoryReg[tid] &= (globalHistoryMask & ~1ULL);
-}//GShareBP::btbUpdate
-
-bool
-GShareBP::lookup(ThreadID tid, Addr branch_addr, void * &bp_history)
-{
-    unsigned index = ((branch_addr^globalHistoryReg[tid])&globalHistoryMask);
-    bool pred = counter[index]>>(ctrBits-1);
-
-    BPHistory *history = new BPHistory;
-    history->globalHistoryReg = globalHistoryReg[tid];
-    history->pred = pred;
-
-    bp_history = static_cast<void*>(history);
-    updateGlobalHistReg(tid, pred);
-    return pred;
-}
-
-void
-GShareBP::update(ThreadID tid, Addr branch_addr, bool taken, void *bp_history,
-                    bool squashed, const StaticInstPtr &inst, Addr corrTarget)
-{
-    assert(bp_history == NULL);
-    BPHistory *history = static_cast<BPHistory*>(bp_history);
-    if (squashed) {
-        globalHistoryReg[tid] = (history->globalHistoryReg << 1) | taken;
-        return;
-    }
-
-    unsigned index = ((branch_addr^(history->globalHistoryReg))
-                    &globalHistoryMask);
-    if (taken) {
-        DPRINTF(Fetch,"Branch updated as taken.\n");
-        counter[index]++;
-    } else {
-        DPRINTF(Fetch, "Branch updated as not taken.\n");
-        counter[index]++;
-    }
-    delete history;
-}
-
-void
-GShareBP::uncondBranch(ThreadID tid, Addr pc, void *&bp_history)
+GShareBP::uncondBranch(ThreadID tid, Addr pc, void * &bpHistory)
 {
     BPHistory *history = new BPHistory;
     history->globalHistoryReg = globalHistoryReg[tid];
     history->pred = true;
-    bp_history = static_cast<void*>(history);
+    bpHistory = static_cast<void*>(history);
     updateGlobalHistReg(tid,true);
+}
+
+void
+GShareBP::squash(ThreadID tid, void *bpHistory)
+{
+    BPHistory *history = static_cast<BPHistory*>(bpHistory);
+    globalHistoryReg[tid] = history->globalHistoryReg;
+
+    delete history;
+
+}
+
+bool
+GShareBP::lookup(ThreadID tid, Addr branchAddr, void * &bpHistory)
+{
+    unsigned counterIdx = (((branchAddr >> instShiftAmt)
+                                ^ globalHistoryReg[tid])
+                                & globalHistoryMask);
+
+    assert(counterIdx < counterSize);
+
+    bool pred = counters[counterIdx]>>(ctrBits-1);
+
+    BPHistory *history = new BPHistory;
+    history->globalHistoryReg=globalHistoryReg[tid];
+    bpHistory = static_cast<void*>(history);
+    updateGlobalHistReg(tid, pred);
+
+    return pred;
+}
+
+void
+GShareBP::btbUpdate(ThreadID tid, Addr branchAddr, void * &bpHistory)
+{
+    globalHistoryReg[tid] &= (historyRegisterMask & ~1ULL);
+}
+
+void
+GShareBP::update(ThreadID tid, Addr branchAddr, bool taken, void *bpHistory,
+                 bool squashed, const StaticInstPtr & inst, Addr corrTarget)
+{
+    assert(bpHistory);
+    BPHistory *history = static_cast<BPHistory*>(bpHistory);
+    if (squashed) {
+        globalHistoryReg[tid] = (history->globalHistoryReg << 1) | taken;
+        return;
+    }
+    unsigned counterIdx = (((branchAddr >> instShiftAmt)
+                                ^ history->globalHistoryReg)
+                                & globalHistoryMask);
+    if (history->pred) {
+        counters[counterIdx]++;
+    }else{
+        counters[counterIdx]--;
+    }
+    delete history;
 }
 
 void
 GShareBP::updateGlobalHistReg(ThreadID tid, bool taken)
 {
-    globalHistoryReg[tid] = taken ? (globalHistoryReg[tid] << 1) | 1 :
+        globalHistoryReg[tid] = taken ? (globalHistoryReg[tid] << 1) | 1 :
                                (globalHistoryReg[tid] << 1);
-    globalHistoryReg[tid] &= globalHistoryMask;
+    globalHistoryReg[tid] &= historyRegisterMask;
 
 }
 
-
-}//branch_prediction
-
-}//gem5
+} // namespace branch_prediction
+} // namespace gem5
--- src/cpu/pred/gshare.hh
+++ src/cpu/pred/gshare.hh
@@ -1,5 +1,5 @@
-#ifndef __CPU_PRED_GSHARE_HH__
-#define __CPU_PRED_GSHARE_HH__
+#ifndef __CPU_PRED_GSHARE_PRED_HH__
+#define __CPU_PRED_GSHARE_PRED_HH__
 
 #include "base/sat_counter.hh"
 #include "cpu/pred/bpred_unit.hh"
@@ -16,13 +16,15 @@ class GShareBP : public BPredUnit
   public:
     GShareBP(const GShareBPParams &params);
     void uncondBranch(ThreadID tid, Addr pc, void * &bp_history);
-    void squashed(ThreadID tid,void *bp_history);
+    void squash(ThreadID tid, void *bp_history);
     bool lookup(ThreadID tid, Addr branch_addr, void * &bp_history);
     void btbUpdate(ThreadID tid, Addr branch_addr, void * &bp_history);
-    void update(ThreadID tid,Addr branch_addr, bool taken, void *bphistory,
-                    bool squashed,const StaticInstPtr & inst, Addr corrTarget);
+    void update(ThreadID tid, Addr branch_addr, bool taken, void *bp_history,
+                bool squashed, const StaticInstPtr & inst, Addr corrTarget);
+
   private:
     void updateGlobalHistReg(ThreadID tid, bool taken);
+
     struct BPHistory
     {
         unsigned globalHistoryReg;
@@ -31,15 +33,15 @@ class GShareBP : public BPredUnit
 
     std::vector<unsigned> globalHistoryReg;
     unsigned globalHistoryBits;
+    unsigned historyRegisterMask;
     unsigned globalHistoryMask;
-    std::vector<SatCounter8> counter;
+
     unsigned ctrBits;
     unsigned counterSize;
-    unsigned indexMask;
+    std::vector<SatCounter8> counters;
 };
 
-}//namespace breanch_prediction
+} // namespace branch_prediction
+} // namespace gem5
 
-}//namespace gem5
-
-#endif // __CPU_PRED_GSHARE_HH__
+#endif // __CPU_PRED_GSHARE_PRED_HH__
